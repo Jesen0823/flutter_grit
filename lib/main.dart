b2d8b1d83103fa077d4fa6ab59bc715d07a499a5
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-29 19:02:49 +0800

    独立出来drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_grit/demo/drawer_demo.dart';
 import 'demo/listview_demo.dart';
 import 'model/post.dart';
 
@@ -60,72 +61,7 @@ class Home extends StatelessWidget {
             Icon(Icons.bike_scooter, size: 128.0, color: Colors.black12)
           ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text(
-                  "Jesen.Mr", style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                accountEmail: Text("kka@test.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.purple,
-                ),
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                  image: DecorationImage(
-                    image: NetworkImage("https://resources.ninghao.net/images/childhood-in-a-picture.jpg"),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                        Colors.yellow[400]!.withOpacity(0.6),
-                        BlendMode.hardLight
-                    )
-                  )
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  "Messages",
-                  textAlign: TextAlign.right,
-                ),
-                // 在左边是leading,右边是trailing
-                trailing: Icon(
-                  Icons.message,
-                  color: Colors.black26,
-                  size: 24.0,
-                ),
-                onTap: () => Navigator.pop(context),
-              ),
-              ListTile(
-                title: Text(
-                  "Favorite",
-                  textAlign: TextAlign.right,
-                ),
-                // 在左边是leading,右边是trailing
-                trailing: Icon(
-                  Icons.favorite,
-                  color: Colors.black26,
-                  size: 24.0,
-                ),
-                onTap: () => Navigator.pop(context),
-              ),
-              ListTile(
-                title: Text(
-                  "Settings",
-                  textAlign: TextAlign.right,
-                ),
-                // 在左边是leading,右边是trailing
-                trailing: Icon(
-                  Icons.settings,
-                  color: Colors.black26,
-                  size: 24.0,
-                ),
-                onTap: () => Navigator.pop(context),
-              )
-            ],
-          ),
-        ),
+        drawer: DrawerDemo(),
       ),
     );
   }
