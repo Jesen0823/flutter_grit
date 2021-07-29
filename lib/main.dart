24bfe496da50ffc614b3edf135edee6748fc800f
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-29 16:34:25 +0800

    添加TabBar标签

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,53 @@ import 'model/post.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      showSemanticsDebugger: false,
-      home: Home(),
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
-      )
-    );
+        showSemanticsDebugger: false,
+        home: Home(),
+        theme: ThemeData(primarySwatch: Colors.yellow));
   }
 }
 
 class Home extends StatelessWidget {
-
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "Navigation",
-            onPressed: () => debugPrint("click Navigation"),
-          ),
-          title: Text('NINGHAO'),
-          actions: [
-            IconButton(
-                onPressed:() => debugPrint("click Search"),
-                icon: Icon(Icons.search),
-              tooltip: "Search",
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            backgroundColor: Colors.grey[100],
+            appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: "Navigation",
+                onPressed: () => debugPrint("click Navigation"),
+              ),
+              title: Text('NINGHAO'),
+              actions: [
+                IconButton(
+                  onPressed: () => debugPrint("click Search"),
+                  icon: Icon(Icons.search),
+                  tooltip: "Search",
+                ),
+              ],
+              elevation: 0.0,
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.local_activity)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.bike_scooter)),
+                ],
+              ),
             ),
-          ],
-          elevation: 0.0,
-        ),
-        body: null
-      );
-    }
+            body: TabBarView(
+              children: [
+                Icon(Icons.local_activity, size: 128.0,color: Colors.black12),
+                Icon(Icons.change_history, size: 128.0,color: Colors.black12),
+                Icon(Icons.bike_scooter, size: 128.0,color: Colors.black12)
+              ],
+            )
+        )
+    );
+  }
 }
-
-
