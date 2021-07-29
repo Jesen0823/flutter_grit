caad4368742f2d2d0b31cfa93b56d34cb47af617
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-29 16:53:05 +0800

    添加左边抽屉Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'demo/listview_demo.dart';
 import 'model/post.dart';
@@ -22,42 +23,54 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            backgroundColor: Colors.grey[100],
-            appBar: AppBar(
-              leading: IconButton(
-                icon: Icon(Icons.menu),
-                tooltip: "Navigation",
-                onPressed: () => debugPrint("click Navigation"),
-              ),
-              title: Text('NINGHAO'),
-              actions: [
-                IconButton(
-                  onPressed: () => debugPrint("click Search"),
-                  icon: Icon(Icons.search),
-                  tooltip: "Search",
-                ),
-              ],
-              elevation: 0.0,
-              bottom: TabBar(
-                unselectedLabelColor: Colors.black45,
-                indicatorColor: Colors.blue,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorWeight: 1.0,
-                tabs: [
-                  Tab(icon: Icon(Icons.local_activity)),
-                  Tab(icon: Icon(Icons.change_history)),
-                  Tab(icon: Icon(Icons.bike_scooter)),
-                ],
-              ),
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Navigation",
+            onPressed: () => debugPrint("click Navigation"),
+          ),
+          title: Text('NINGHAO'),
+          actions: [
+            IconButton(
+              onPressed: () => debugPrint("click Search"),
+              icon: Icon(Icons.search),
+              tooltip: "Search",
             ),
-            body: TabBarView(
-              children: [
-                Icon(Icons.local_activity, size: 128.0, color: Colors.black12),
-                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-                Icon(Icons.bike_scooter, size: 128.0, color: Colors.black12)
-              ],
-            )));
+          ],
+          elevation: 0.0,
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black45,
+            indicatorColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorWeight: 1.0,
+            tabs: [
+              Tab(icon: Icon(Icons.local_activity)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.bike_scooter)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.local_activity, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.bike_scooter, size: 128.0, color: Colors.black12)
+          ],
+        ),
+        drawer: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("This is a Drawer")
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
