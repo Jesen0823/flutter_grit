e0eaf667850397010e238db21f604981a93a1254
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-29 16:18:18 +0800

    设置AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'demo/listview_demo.dart';
 import 'model/post.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      showSemanticsDebugger: false,
       home: Home(),
       theme: ThemeData(
         primarySwatch: Colors.yellow
@@ -16,57 +19,30 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead
-          ),
-          SizedBox(height: 16.0),
-        ],
-      ),
-    );
-  }
 
   @override
     Widget build(BuildContext context) {
       return Scaffold(
         backgroundColor: Colors.grey[100],
         appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Navigation",
+            onPressed: () => debugPrint("click Navigation"),
+          ),
           title: Text('NINGHAO'),
+          actions: [
+            IconButton(
+                onPressed:() => debugPrint("click Search"),
+                icon: Icon(Icons.search),
+              tooltip: "Search",
+            ),
+          ],
           elevation: 0.0,
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        )
+        body: null
       );
     }
 }
 
-class Hello extends StatelessWidget {
-  @override
-    Widget build(BuildContext context) {
-      return Center(
-        child: Text(
-          'hello',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black87,
-          )
-        ),
-      );
-    }
-}
+
