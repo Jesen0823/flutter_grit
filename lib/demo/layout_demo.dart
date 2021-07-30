3ad48dc45d6fc131a0f5b1ccc4c03a39cfb25c07
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-30 12:08:01 +0800

    SizedBox一般用作间隔，也可以包裹内容

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -4,9 +4,76 @@ class LayoutDemo extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: [
+          UseSizedBox(),
+          UseAxisAlignment(),
+        ],
+      )
+    );
+  }
+}
+
+class UseSizedBox extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconBadge(Icons.bike_scooter)
+        // SizedBox 如果不设置大小，默认是它包裹内容的大小,常用来做间隔
+        SizedBox(
+          width: 80.0,
+          height: 120.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Icon(
+              Icons.add_location_alt_sharp,
+              color: Colors.white,
+              size: 32.0,
+            ),
+          ),
+        ),
+        // 此处用作间隔
+        SizedBox(
+          height: 20.0,
+        ),
+        SizedBox(
+          width: 100.0,
+          height: 80.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(40, 100, 90, 1.0),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Icon(
+              Icons.wb_sunny,
+              color: Colors.white,
+              size: 32.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// 主轴，交叉轴的使用
+class UseAxisAlignment extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      // 主轴位置
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      // 交叉轴位置，与主轴垂直的轴
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        IconBadge(Icons.pool),
+        IconBadge(Icons.umbrella,size: 48.0,),
+        IconBadge(Icons.face),
       ],
     );
   }
