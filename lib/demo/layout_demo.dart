4de904e45f53d093ecc33b0a6ba20a4c104cfa26
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-30 11:12:25 +0800

    add 自定义layout,自定义小部件IconBadge

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -4,6 +4,32 @@ class LayoutDemo extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        IconBadge(Icons.bike_scooter)
+      ],
+    );
+  }
+}
+
+// 自定义控件
+class IconBadge extends StatelessWidget{
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon,{this.size = 32.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(
+        icon,
+        size: size,
+        color: Colors.white,
+      ),
+      width: size+60,
+        height: size+60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
   }
 }
