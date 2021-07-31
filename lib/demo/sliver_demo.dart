79f193d7bcd6f8a72c9e8d944be10d7d79be5fc5
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-31 17:30:14 +0800

    使用SliverGrid创建CustomScrollView，一个网格视图

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_grit/model/post.dart';
 
 class SliverDemo extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index){
+                    return Container(
+                      child: Image.network(
+                        posts[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                childCount: posts.length,
+              ),
+
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+                childAspectRatio: 1.7,
+              ),
+          ),
+        ],
+      ),
+    );
   }
 }
