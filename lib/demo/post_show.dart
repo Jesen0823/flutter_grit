1d651ab06a3d7b7d13f283d991098038f49770ed
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-07-31 22:40:45 +0800

    详情页内容编辑，

--- lib/demo/post_show.dart
+++ lib/demo/post_show.dart
@@ -4,12 +4,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_grit/model/post.dart';
 
-class PostShow extends StatelessWidget{
+class PostShow extends StatelessWidget {
   final Post post;
 
   PostShow({
-   required this.post,
-});
+    required this.post,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +17,37 @@ class PostShow extends StatelessWidget{
       appBar: AppBar(
         title: Text('${post.title}'),
         elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Image.network(post.imageUrl),
+          Container(
+            padding: EdgeInsets.all(32.0),
+            // 占满所有可用宽度
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${post.title}',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text(
+                  '${post.author}',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                // 分割空白
+                SizedBox(
+                  height: 32.0,
+                ),
+                Text(
+                  '${post.description}',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,10 +1,14 @@
 class Post {
   const Post(
-      {required this.title, required this.author, required this.imageUrl});
+      {required this.title,
+      required this.author,
+      required this.imageUrl,
+      required this.description});
 
   final String title;
   final String author;
   final String imageUrl;
+  final String description;
 }
 
 final List<Post> posts = [
@@ -12,75 +16,90 @@ final List<Post> posts = [
     title: 'Candy Shop',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/candy-shop.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Childhood in a picture',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/childhood-in-a-picture.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Contained',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/contained.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Dragon',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/dragon.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Free Hugs',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/free_hugs.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Gravity Falls',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/gravity-falls.png',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Icecream Truck',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/icecreamtruck.png',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'keyclack',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/keyclack.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Overkill',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/overkill.png',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Say Hello to Barry',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/say-hello-to-barry.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Space Skull',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/space-skull.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'The Old Fashioned',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/the-old-fashioned.png',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Tornado',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/tornado.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'Undo',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/undo.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   ),
   Post(
     title: 'White Dragon',
     author: 'Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.net/images/white-dragon.jpg',
+    description: 'It gives error reading dependency file and then flutter crashes. Is it something to do with dart to or am i missing something here?',
   )
 ];
