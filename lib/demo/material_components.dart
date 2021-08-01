2d1647f26232e986d5c54c7b7a4f30ef8f084d8f
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-01 15:41:56 +0800

    FloatingActionButton

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -43,8 +43,50 @@ class ListItem extends StatelessWidget {
 
 // 第一个button案例：FloatingActionButton
 class FloatingActionButtonDemo extends StatelessWidget {
+  final Widget _floatingActionButton = FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black12,
+    shape: BeveledRectangleBorder(
+      borderRadius: BorderRadius.circular(30.0),
+    ),
+  );
+
+  final Widget _floatingActionButtonNoShape = FloatingActionButton(
+    onPressed: () {},
+    child: Icon(Icons.add),
+    elevation: 0.0,
+    backgroundColor: Colors.black12,
+  );
+
+  final Widget _floatingActionButtonExtended = FloatingActionButton.extended(
+    onPressed: () {},
+    label: Text('Add'),
+    icon: Icon(Icons.add),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _floatingActionButtonExtended,
+          SizedBox(
+            height: 32.0,
+          ),
+          _floatingActionButton,
+          SizedBox(
+            height: 32.0,
+          ),
+          _floatingActionButtonNoShape,
+        ],
+      ),
+    );
   }
 }
