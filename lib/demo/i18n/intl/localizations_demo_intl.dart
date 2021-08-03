9a69689093db6a4afbc08c1c5cccc5faa78acbd4
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-03 23:56:08 +0800

    生成翻译模板.arb文件

--- lib/demo/i18n/intl/localizations_demo_intl.dart
+++ lib/demo/i18n/intl/localizations_demo_intl.dart
@@ -1,9 +1,13 @@
 import 'package:intl/intl.dart';
+//命令：
+/*flutter pub pub run intl_tr
+anslation:extract_to_arb --output-dir=lib/demo/i18n/intl/ lib/demo/i18n/
+intl/localizations_demo_intl.dart*/
 
 class LocalizationDemoIntl {
   String get title => Intl.message(
     'hello',
-    name: 't',
+    name: 'title',
     desc: 'demo localizations',
   );
 
